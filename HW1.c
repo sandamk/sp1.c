7ed87afacf69d35c8a1c710dfb1cd823d2d0498e
sandamk <98730926+sandamk@users.noreply.github.com>
2022-01-31 00:23:09 -0600

    Add files via upload

---
+++ HW1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct keys{
+    char keyword[10];
+    int count;
+};
+//initializing the table
+void init_table(struct keys *s, int n)
+{
+    for (int i =0;i<n;i++)
+    {
+        (s+i)->count = 0;
+        strcpy((s+i)->keyword," ");
+    }
+}
+//incremeting the count
+void update_table(struct keys *s, int n)
+{
+    (s+n)->count++;
+}
+//Displaying the table
+void display_table(struct keys *s, int n)
+{
+    for (int i = 0; i < n; i++)
+    printf ("Name: %s\t count:%d\n", (s + i)->keyword, (s + i)->count);
+}
+int main(int argc, char* argv[])
+{
+    struct keys *ptr;
+    char *str=NULL;
+    char *split;
+    const char s[2]=" ";
+    ptr = (struct keys *) malloc (argc * sizeof (struct keys));
+    init_table(ptr,argc); //intialize the keywords and counts
+    for (int i =0; i< argc; i++){
+        printf("%s\n",argv[i]);
+        strcpy((ptr+i)->keyword,argv[i]);
+    }
+  char *line = NULL;
+  size_t maxlen = 0;
+  ssize_t t;
+
+  /* read the user input using the getlin() until the NULL is found  */
+  while ((t = getline (&line, &maxlen, stdin)) > 0)
+    {
+        split = strtok (line, " "); //Split the word whenever a space is encountered
+    while (split != NULL)
+    {
+      for (int i = 0; i <= argc; i++)
+	{
+    /*Comparing the split word with the keywords and if a match is found, increment the corresponding count */
+	  if (strcmp (split, (ptr + i)->keyword) == 0)
+	    {
+	      update_table(ptr,i);
+	    }
+	}
+      split = strtok (NULL, " ,.\n");
+    }
+    }
+    display_table(ptr,argc); //Display the keywords and counts
+    free (line);
+    return 0;
+}
